1e6ae7f5f33881042add4aba71a5576e88f191b5
Prathap Selvakumar <prathapsk@hotmail.com>
2025-10-16 22:05:46 +0100

    Addition of videos

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -158,7 +158,7 @@ export default function ProjectsSection() {
                       preload="none"
                       poster=""
                     >
-                      <source src="/src/components/Audio Search Engine.mp4" type="video/mp4" />
+                      <source src="public\Audio Search Engine.mp4" type="video/mp4" />
                       Your browser does not support the video tag.
                     </video>
                   </div>
